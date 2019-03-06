621e466bbd71b9cbd0310babb07a7cd0751fadc6
Başak SEÇMEN 502516 <yr502516@adalet.gov.tr>
2019-03-06 14:02:50 +0300

    login.blade edited

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Kullanıcı Adı') }}</label>
 
                             <div class="col-md-6">
